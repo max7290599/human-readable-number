53560fc178247550f750572bc5fbaf4ee4a49a25
max7290599 <atroshschanka.maksim@gmail.com>
2021-03-13 00:06:10 +0300

    refactor: change structure of the task

--- src/index.js
+++ src/index.js
@@ -1,30 +1,27 @@
-let A1 = ['', "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
-let A2 = ["eleven", "twelve", "thirteen", "fourteen",
+const UNITS = ['', "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen",
     "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"
-];
+]
+const DOZENS = ['', 'ten', 'twenty', 'thirty', 'forty', 'fifty', "sixty", "seventy", "eighty", "ninety"];
 
-let A3 = ['ten', 'twenty', 'thirty', 'forty', 'fifty', "sixty", "seventy", "eighty", "ninety"];
-
-let A4 = ['one hundred', 'two hundred', 'three hundred', 'four hundred', 'five hundred',
+const HUNDREDS = ['', 'one hundred', 'two hundred', 'three hundred', 'four hundred', 'five hundred',
     'six hundred', 'seven hundred', 'eight hundred', 'nine hundred'
 ];
 module.exports = function toReadable(n) {
-    let b = n % 10;
-    let a = (n - b) / 10;
-    let c = Math.floor(n / 100);
-    let d = Math.floor((n / 10) % 10);
+    const b = n % 10;
+    const a = (n - b) / 10;
+    const c = Math.floor(n / 100);
+    const d = Math.floor((n / 10) % 10);
 
     if (n == 0) return 'zero';
     if (n > 99) {
-        if (n % 100 > 10 && n % 100 < 20) return A4[c - 1] + ' ' + A2[n % 100 - 11];
+        if (b == 0 && d == 0) return HUNDREDS[c];
+        if (n % 100 < 20) return HUNDREDS[c] + ' ' + UNITS[n % 100];
+        if (b == 0) return HUNDREDS[c] + ' ' + DOZENS[d]
+        if (d == 0) return HUNDREDS[c] + ' ' + UNITS[b];
+        return HUNDREDS[c] + ' ' + DOZENS[d] + ' ' + UNITS[b]
     }
-    if (c && b == 0 && d == 0) return A4[c - 1];
-    if (c && b == 0) return A4[c - 1] + ' ' + A3[d - 1]
-    if (c && d == 0) return A4[c - 1] + ' ' + A1[b];
-    if (c) return A4[c - 1] + ' ' + A3[d - 1] + ' ' + A1[b]
-    if (n < 10) return A1[n]
-    if (n > 10 && n < 20) return A2[n - 11];
-    if (b == 0) return A3[a - 1];
-    return A3[a - 1] + ' ' + A1[b];
+    if (n < 20) return UNITS[n];
+    if (b == 0) return DOZENS[a];
+    return DOZENS[a] + ' ' + UNITS[b];
 
 }
